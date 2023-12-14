562e2b178886fc5a03cbf273acc61197bd444dd6
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-12-14 16:51:16 +0100

    Update card designs

--- frontend/components/CharacterCard.jsx
+++ frontend/components/CharacterCard.jsx
@@ -1,12 +1,14 @@
+import './styles/CharacterCard.css';
 
 const CharacterCard = ({characterName, characterImage, onClick}) => {
   return (
-    <>
+    <div className="card_container">
       <div className="character_container" onClick={onClick}>
-        <img className="character_image" src={characterImage} alt="characterName" />
         <p className="character_name">{characterName}</p>
+        <img className="character_image" src={characterImage} alt="characterName" />
+        <p className="character_quote">"People said I was dumb, but I proved them."</p>
       </div>
-    </>
+    </div>
   )
 }
 
