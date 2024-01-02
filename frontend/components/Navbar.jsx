c665d86998270c00bac2f4b67f59f1afd92260a4
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2024-01-02 17:58:36 +0100

    Update leaderboard button to show leaderboard from navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import FuturamaIcon from '/icon.png';
 import Leaderboard from './Leaderboard';
 import './styles/Navbar.css';
 
 function Navbar({ wins, handleGoBack, scores }) {
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+
+  const leaderboardVisbility = () => {
+    setShowLeaderboard(prevState => !prevState);
+  }
+
   return (
     <div className="navbar_container">
       <img onClick={handleGoBack} className="futurama_icon" src={FuturamaIcon} alt="icon" />
       <div>
         <p className="game_wins" >Hard win streak: {wins}</p>
-        <button>Leaderboard</button>
+        <button onClick={leaderboardVisbility}>Leaderboard</button>
       </div>
-      <Leaderboard scores={scores} />
+      { showLeaderboard &&  <Leaderboard scores={scores} /> }
     </div>
   );
 }
